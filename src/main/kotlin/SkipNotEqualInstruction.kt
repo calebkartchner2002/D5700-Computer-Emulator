19e94d3f76f3ca4583d2883b50ebb18c0eda8c6b
Caleb Kartchner <calebkartchner2002@gmail.com>
2025-08-01 20:53:49 -0600

    removing dumb comments

--- src/main/kotlin/SkipNotEqualInstruction.kt
+++ src/main/kotlin/SkipNotEqualInstruction.kt
@@ -10,11 +10,8 @@ class SkipNotEqualInstruction(byte1: Byte, byte2: Byte) : Instruction(byte1, byt
         val x = getRegisterX()
         val y = getRegisterY()
 
-        // If not equal, skip the *next* instruction: oldPC + 4.
-        // Else do nothing; CPU will auto-increment +2 after this method.
         if (registers.general[x] != registers.general[y]) {
             registers.setPC(registers.programCounter + 4)
         }
     }
-    // keep default shouldIncrementPC() behavior from base Instruction
 }
